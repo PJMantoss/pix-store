4695028eafbc6c374c35736bc0c1eb57bfc23927
pjmantoss <joelptoss@gmail.com>
2020-12-01 13:26:28 +0100

    update Context

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,3 +1,13 @@
 import React, {createContext} from 'react';
 
 const AppContext = createContext();
+
+function AppContextProvider(){
+    return(
+        <Provider value="">
+            {}
+        </Provider>
+    )
+};
+
+export default {AppContextProvider, AppContext};
