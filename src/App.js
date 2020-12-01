f1ca6662e37d3bd1360bc676c90c9738e84ea5fa
pjmantoss <joelptoss@gmail.com>
2020-12-01 13:11:16 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react"
 import Header from './components/Header';
 import Photos from './pages/Photos';
 import Cart from "./pages/Cart";
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+
+      <Switch>
+        <Route path=""></Route>
+
+        <Route path=""></Route>
+      </Switch>
     </div>
   );
 }
