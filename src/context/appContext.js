296274e772b33304e95e790c86aa9653ac171b72
pjmantoss <joelptoss@gmail.com>
2020-12-01 14:12:50 +0100

    update context

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -5,7 +5,7 @@ const AppContext = createContext();
 function AppContextProvider(props){
     const [photos, setPhotos] = useState([]);
 
-    const url = "";
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
 
     useEffect(() => {
         fetch().then().then().catch(err => console.error(err))
