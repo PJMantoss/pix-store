77eb377e7dd74c43e994ddacf25121ceaa726ca2
pjmantoss <joelptoss@gmail.com>
2020-12-01 13:03:06 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from './components/Header';
-import Photos from './pages/Photos'
+import Photos from './pages/Photos';
+import Cart from "./pages/Cart";
 import './App.css';
 
 function App() {
