7f3d282fa0d70514ed0fab8d5af978284e33c208
pjmantoss <joelptoss@gmail.com>
2020-12-03 16:14:32 +0100

    update Image

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Image from '../components/Image';
 import {AppContext} from '../context/appContext';
+import {getClass} from '../utils';
 
 function Photos(){
+    const [allPhotos] = useContext(AppContext);
     return(
         <main className="photos">
             <h1>Images Go here</h1>
