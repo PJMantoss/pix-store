e944d84cc461b97edfe0f1d9ef32e30892bf43be
pjmantoss <joelptoss@gmail.com>
2020-12-03 16:01:07 +0100

    update Image

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,7 @@ import React from "react";
 function Image({className}){
     return(
         <div className={`${className} image-container`}>
-            <img src={} className="image-grid" />
+            <img src={img.url} className="image-grid" />
         </div>
     );
 }
