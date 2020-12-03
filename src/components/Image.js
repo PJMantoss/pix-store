9dfa0a984ae8d402ef3d072b11b3693056555876
pjmantoss <joelptoss@gmail.com>
2020-12-03 16:59:49 +0100

    update Image

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 function Image({className, img}){
     const [hovered, setHovered] = useState(false);
     return(
-        <div onMouseEnter={} onMouseLeave={} className={`${className} image-container`}>
+        <div onMouseEnter={() => setHovered(true)} onMouseLeave={} className={`${className} image-container`}>
             <img src={img.url} className="image-grid" />
         </div>
     );
