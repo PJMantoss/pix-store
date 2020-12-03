9e69400852ac51f1a462c7e6e7d9ac0851810d75
pjmantoss <joelptoss@gmail.com>
2020-12-03 16:58:23 +0100

    update Image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Image({className, img}){
+    const [hovered, setHovered] = useState(false);
     return(
         <div className={`${className} image-container`}>
             <img src={img.url} className="image-grid" />
