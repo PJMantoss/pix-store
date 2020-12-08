95d694ded46d0b990f3c38ca21ff152ecab15925
pjmantoss <joelptoss@gmail.com>
2020-12-08 08:57:11 +0100

    update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ function Header(){
             <header>
                 <Link to="/"><h2>Pix Store</h2></Link>
                 <Link to="/cart">
-                    <i className={`${} ri-fw ri-2x`}></i>
+                    <i className={`${cartClassName} ri-fw ri-2x`}></i>
                 </Link>
             </header>
         </div>
