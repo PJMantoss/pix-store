136ac98c001fb0173c4b2a488cbd8e82e7dcf4ac
pjmantoss <joelptoss@gmail.com>
2020-12-08 08:55:44 +0100

    update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,14 @@ import "../App.css"
 function Header(){
     const {cartItems} = useContext(AppContext);
 
-    const cartClassName = cartItems.length > 0 ? <i className="ri-shopping-cart-fill ri-fw ri-2x"></i> :
+    const cartClassName = cartItems.length > 0 ? <i className="ri-shopping-cart-fill ri-fw ri-2x"></i> : <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
    
     return(
         <div>
             <header>
                 <Link to="/"><h2>Pix Store</h2></Link>
                 <Link to="/cart">
-                    {cartIcon()}
+                <i className="ri-shopping-cart-fill ri-fw ri-2x"></i>
                 </Link>
             </header>
         </div>
