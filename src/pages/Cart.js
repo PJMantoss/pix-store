f3acaf781284d84f4c9be16e927ec297a3fdfe00
pjmantoss <joelptoss@gmail.com>
2020-12-09 09:22:44 +0100

    update Cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import {AppContext} from "../context/appContext";
 
 function Cart(){
+    const {cartItems} = useState(AppContext);
+
     return(
         <main className="cart-page">
             <h1>Checkout</h1>
