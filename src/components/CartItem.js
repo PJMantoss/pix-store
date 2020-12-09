35dbf586ca2b3f2bf8fb54f3e872c71e770d8732
pjmantoss <joelptoss@gmail.com>
2020-12-09 09:45:08 +0100

    update CartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,9 +3,9 @@ import React from "react";
 function CartItem({item}){
     return(
        <div className="cart-item">
-           <i className=""></i>
+           <i className="ri-delete-bin-line"></i>
            <img src={item.url} width="130px" />
-           <p></p>
+           <p>$6.00</p>
        </div>
     )
 }
