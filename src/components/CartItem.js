559fd7274e3e20c588cc089d763b28cb247ee7bf
pjmantoss <joelptoss@gmail.com>
2020-12-09 09:44:06 +0100

    update CartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,8 +3,8 @@ import React from "react";
 function CartItem({item}){
     return(
        <div className="cart-item">
-           <i></i>
-           <img />
+           <i className=""></i>
+           <img src={item.url} width="130px" />
            <p></p>
        </div>
     )
