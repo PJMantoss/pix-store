1f89f25735d4fe923b77e6975c1171890ecd51ba
pjmantoss <joelptoss@gmail.com>
2020-12-09 16:21:40 +0100

    update custom hook

--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -2,6 +2,11 @@ import {useState} from "react"
 
 function useHover(){
     const [hovered, setHovered] = useState(false);
+
+    const enter = () => {
+        setHovered(true);
+    }
+    
 }
 
 export default useHover;
