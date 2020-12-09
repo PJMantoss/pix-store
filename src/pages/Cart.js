58335695cf8b7ce554991b01994f58026c564889
pjmantoss <joelptoss@gmail.com>
2020-12-09 09:54:43 +0100

    update App.css

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -13,7 +13,7 @@ function Cart(){
         <main className="cart-page">
             <h1>Checkout</h1>
             {cartItemElements}
-            <p className="total-cost"></p>
+            <p className="total-cost">Total: </p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>
