655ea283cf9a782d23ff374c4d116950a289d728
pjmantoss <joelptoss@gmail.com>
2020-12-09 13:43:20 +0100

    update CartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -5,7 +5,7 @@ function CartItem({item}){
     const {removeFromCart} = useContext(AppContext);
     return(
        <div className="cart-item">
-           <i className="ri-delete-bin-line"></i>
+           <i className="ri-delete-bin-line" onClick={() => removeFromCart(item.id)}></i>
            <img src={item.url} width="130px" />
            <p>$6.00</p>
        </div>
