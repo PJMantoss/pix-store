cc00adfc9731733c91951d1934cacb9378bed8cb
pjmantoss <joelptoss@gmail.com>
2020-12-13 17:42:11 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Photos from './pages/Photos';
 import Cart from "./pages/Cart";
 import {Switch, Route} from 'react-router-dom';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
